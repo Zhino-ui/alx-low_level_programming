770b00601657780e6d35fa559f5f2b411d81e814
Maxwell Ofori Asante <zhino4u2010@gmail.com>
2023-02-17 06:15:15 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,35 @@
+/*
+ * File: 10-print_comb2.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- *main - Entry point, print 00 to 99 using putchar
- *Return: Always 0 (Success)
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int tens;
-	int ones;
+	int digit1, digit2;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print tens place*/
+	for (digit1 = 0; digit1 < 10; digit1++)
 	{
-		for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
+		for (digit2 = 0; digit2 < 10; digit2++)
 		{
-			putchar(tens);
-			putchar(ones);
-			if (!(tens == '9' && ones == '9')) /*skip comma at end*/
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 9 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
