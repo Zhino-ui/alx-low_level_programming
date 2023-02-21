d047eb10426550e0695f8df3ae42aeba17e96e7e
Maxwell Ofori Asante <zhino4u2010@gmail.com>
2023-02-21 15:24:30 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,16 @@
-#include "_putchar.h"
-
-/**
- * main - print _putchar, followed by new line
- * Return: 0
- */
+#include "main.h"
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
-
