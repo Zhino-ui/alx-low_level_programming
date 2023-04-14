4f5b50143c1db77da45396256cbb53a54e384e57
Maxwell Ofori Asante <zhino4u2010@gmail.com>
2023-04-14 05:17:50 -0700

    cat hello

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,54 +1,32 @@
 #include "main.h"
 
 /*
- * _strlen - find string length
- * @string: string
- * Return: value of string
- */
-
-int _strlen(char *str)
-{
-	int len;
-
-	for (len = 0; str[len] != '\0';)
-	{
-		len++;
-	}
-	return (len);
-}
-
-/*
- * append_text_to_file - appends text to file if exits
- * @filename: name of file
- * @text_content: contents of a file
- * Return: prints 1 on success and -1 on fail
+ * append_text_to_file - appends text to file
+ * @filename: file
+ * @text_content : text content
+ * Return:-1 if fail or NULL else 1
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int td;
-	int w;
+	int op, wi, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	td = open(filename, O_WRONLY | O_APPEND);
-	if (td == -1)
+	if (text_content != NULL)
 	{
-		return (-1);
-	}
-	if (!text_content)
-	{
-		close(td);
-		return (1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	w = write(td, text_content, _strlen(text_content));
-	if (w == -1 || w != _strlen(text_content))
-	{
-		close(td);
+	op = open(filename, O_WRONLY | O_APPEND);
+	wi = write(op, text_content, len);
+
+	if (op == -1 || wi == -1)
 		return (-1);
-	}
-	close(td);
+
+	close(op);
+
 	return (1);
 }
