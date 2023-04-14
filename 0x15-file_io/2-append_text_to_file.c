50ff8850bc16f0673827c8ee82ab06580e2d07c2
Maxwell Ofori Asante <zhino4u2010@gmail.com>
2023-04-14 06:25:01 -0700

    echo -n Hello > hello

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,32 +1,51 @@
 #include "main.h"
 
 /*
- * append_text_to_file - appends text to file
- * @filename: file
- * @text_content : text content
- * Return:-1 if fail or NULL else 1
+ * _strlength- length of string
+ * @str: string
+ * Retuen: value
+ */
+
+int _strlength(char *str)
+{
+	int length;
+
+	for (length = 0; str[length] != '\0'; length++)
+	return (length);
+}
+
+/*
+ * append_text_to_file- Appends text
+ * @filename: filename
+ * @text_content: text content
+ * Return: 1 success -1 fail
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int op, wi, len = 0;
+	int td;
+	int w;
 
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
+	if (!filename)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		return (-1);
 	}
 
-	op = open(filename, O_WRONLY | O_APPEND);
-	wi = write(op, text_content, len);
-
-	if (op == -1 || wi == -1)
+	td = open(filename | O_WRONLY | O_APPEND);
+	if (td == -1)
+	{
 		return (-1);
+	}
 
-	close(op);
+	if (!text_content)
+	{
+		close(td);
+		return (1);
+	}
 
-	return (1);
-}
+	w = write(td, text_content, _strlength(text_contet));
+	if (w == -1 || w != _strlength(text_content))
+	{
+		close(td);
+		return (-1);
+	}
